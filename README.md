dd2c8e77c57f91af1eb39969ea0f6eaee1972eb0
Charles Muchene <charlesmuchene@gmail.com>
2019-12-16 11:35:48 -0600

    Commit ya kwanza

---
+++ README.md
@@ -0,0 +1,7 @@
+# Finco
+Financial Company
+
+## Members
+* Charles Muchene | 986817
+* Raul Alvarez | 986975
+* Dany Gamara | 610491
---
+++ src/edu/miu/asd/finco/Finco.java
@@ -0,0 +1,8 @@
+package edu.miu.asd.finco;
+
+public class Finco {
+
+    public static void main(String[] args) {
+        System.out.println("Financial Company");
+    }
+}

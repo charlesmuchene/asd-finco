bc1b3cebefdd6c830bb59f702627baeeb5120589
Charles Muchene <charlesmuchene@gmail.com>
2019-12-16 18:17:47 -0600

    Abstracts factories to finco class

--- src/edu/miu/asd/finco/Finco.java
+++ src/edu/miu/asd/finco/Finco.java
@@ -10,26 +10,56 @@ import edu.miu.asd.finco.factories.*;
 
 public class Finco {
 
+    private AbstractCustomerFactory customerFactory;
+    private AbstractAccountFactory accountFactory;
+    private AbstractEntryFactory entryFactory;
+
+    public Finco() {
+        this.customerFactory = new CustomerFactory();
+        this.accountFactory = new AccountFactory();
+        this.entryFactory = new EntryFactory();
+    }
+
+    public void setAccountFactory(AbstractAccountFactory accountFactory) {
+        this.accountFactory = accountFactory;
+    }
+
+    public void setCustomerFactory(AbstractCustomerFactory customerFactory) {
+        this.customerFactory = customerFactory;
+    }
+
+    public void setEntryFactory(AbstractEntryFactory entryFactory) {
+        this.entryFactory = entryFactory;
+    }
+
+    public AbstractCustomerFactory getCustomerFactory() {
+        return customerFactory;
+    }
+
+    public AbstractAccountFactory getAccountFactory() {
+        return accountFactory;
+    }
+
+    public AbstractEntryFactory getEntryFactory() {
+        return entryFactory;
+    }
+
     public static void main(String[] args) {
         System.out.println("Financial Company");
         System.out.println("-----------------------------");
 
-//        Instantiate default functionality
-        AbstractCustomerFactory customerFactory = new CustomerFactory();
-        AbstractAccountFactory accountFactory = new AccountFactory();
-        AbstractEntryFactory entryFactory = new EntryFactory();
+        Finco finco = new Finco();
 
-        ICustomer customer = customerFactory.createCustomer();
-        IAccount account = accountFactory.createAccount();
-        Card card = accountFactory.createCard();
-        IEntry entry = entryFactory.createEntry();
-
-        account.setCard(card);
+        ICustomer customer = finco.getCustomerFactory().createCustomer();
+        IAccount account = finco.getAccountFactory().createAccount();
+        IEntry entry = finco.getEntryFactory().createEntry();
+        Card card = finco.getAccountFactory().createCard();
 
         card.execute(entry);
-
+        account.setCard(card);
         customer.addAccount(account);
 
+        // Persistence
         FinCoDao finCoDao = new InMemoryFinCoDao();
         finCoDao.saveAccount(account);
         finCoDao.saveCustomer(customer);

a0f42f875ca9f58ff9cbf7936968b68ecc6af12b
Charles Muchene <charlesmuchene@gmail.com>
2019-12-18 09:25:05 -0600

    Fixes notify customer logic

--- src/edu/miu/asd/finco/bank/BankApplication.java
+++ src/edu/miu/asd/finco/bank/BankApplication.java
@@ -2,11 +2,9 @@ package edu.miu.asd.finco.bank;
 
 import edu.miu.asd.finco.bank.controllers.AccountTypeController;
 import edu.miu.asd.finco.bank.controllers.InterestController;
-import edu.miu.asd.finco.bank.domain.AccountType;
 import edu.miu.asd.finco.bank.factories.BankAccountFactory;
 import edu.miu.asd.finco.framework.FincoApplication;
 import edu.miu.asd.finco.framework.controllers.AccountController;
-import edu.miu.asd.finco.framework.factories.AbstractAccountFactory;
 
 public class BankApplication extends FincoApplication {
 
@@ -23,8 +21,6 @@ public class BankApplication extends FincoApplication {
 
         this.bankForm = new BankForm(new InterestController(getDao()), new AccountTypeController(bankAccountFactory));
         setApplicationForm(bankForm);
-
-//        this.bankAccountFactory.setAccountType(AccountType.SAVINGS);
 
         this.setAccountFactory(bankAccountFactory);
         this.bankForm.setAccountController(this.accountController);
